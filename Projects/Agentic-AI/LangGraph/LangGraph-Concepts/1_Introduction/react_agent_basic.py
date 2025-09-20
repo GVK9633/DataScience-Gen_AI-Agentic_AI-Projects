153652fa105ae35e903bff8c547591b7fac42cc9
GVK9633 <gvijaykumarachary@gmail.com>
2025-09-20 11:59:43 +0530

    langgraph concepts.

---
+++ Projects/Agentic-AI/LangGraph/LangGraph-Concepts/1_Introduction/react_agent_basic.py
@@ -0,0 +1,37 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.agents import initialize_agent, tool
+from langchain_community.tools import TavilySearchResults
+from langchain_openai import ChatOpenAI
+import datetime
+import os
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = "s" 
+print(os.environ.get("OPENAI_API_KEY"))
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
+
+# result = llm.invoke("Who is pm of india in one word")
+# print(result)
+# llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+# os.environ["TAVILY_API_KEY"] = ""
+# search_tool = TavilySearchResults(search_depth="basic")
+
+# @tool
+# def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
+#     """ Returns the current date and time in the specified format """
+
+#     current_time = datetime.datetime.now()
+#     formatted_time = current_time.strftime(format)
+#     return formatted_time
+
+
+# tools = [search_tool, get_system_time]
+
+# agent = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True)
+
+# agent.invoke("When was SpaceX's last launch and how many days ago was that from this instant")
+
